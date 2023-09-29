52b95eb3edd3d448b03a0bbce5333835e8495773
Mario2 <94698816+mariopl2020@users.noreply.github.com>
2023-09-29 22:43:36 +0200

    Operatory matematyczne, konwersja typow

---
+++ main.py
@@ -0,0 +1,38 @@
+
+print("Kolejność: ")
+print((2 + 2) * 2)
+
+print("Dzielenie: ")
+print(5 / 2)
+print(5 // 2) #dzielenie bez reszty
+
+print("Mnożenie: ")
+print(2 * 3)
+print("Potęgowanie")
+print(2 ** 3)
+
+print("Skrócone operatory: ")
+x = 5
+x = x+1
+print(x)
+x += 1
+print(x)
+#brak inkrementacji w języku python x++
+
+print("Konwersja typow: ")
+a = input("Podaj 1 liczbę: ") #input ma standardowo zaimplementowany String
+b = input("Podaj 2 liczbę: ")
+# print(int(a) + b) #to wygeneruje błąd
+print(int(a) + int(b)) #uzycie funkcji int
+print(float(a) + float(b)) # dla liczb zmiennoprzecinkowych
+y = 2
+z = 3
+print(y + z)
+print(str(y) + str(z)) #konwersja na string
+
+del y #pozbywanie się zmiennej. Przez poniższe wygenerował się błąd
+#print(str(y) + str(z))
+
+#grupowe przypisanie wartości zmiennej
+a=b=c = 2
+print(a, b, c)
