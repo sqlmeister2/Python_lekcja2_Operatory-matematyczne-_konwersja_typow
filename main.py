c985008921d63892858f44c7d9d5ba5ca8458226
MarioPC <sqlmeister2@gmail.com>
2024-10-23 21:37:45 +0200

    zakomentowanie kodów w celu uruchamiania kodów częsciami

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 
-print("Kolejność: ")
-print((2 + 2) * 2)
+# print("Kolejność wykonywania operacji arytmetycznych: ")
+# print((2 + 2) * 2)
 
-print("Dzielenie: ")
-print(5 / 2)
-print(5 // 2) #dzielenie bez reszty
+# print("Dzielenie: ")
+# print(5 / 2)
+# print(5 // 2) #dzielenie bez reszty
 
-print("Mnożenie: ")
-print(2 * 3)
-print("Potęgowanie")
-print(2 ** 3)
+# print("Mnożenie: ")
+# print(2 * 3)
+# print("Potęgowanie")
+# print(2 ** 3)
 
-print("Skrócone operatory: ")
-x = 5
-x = x+1
-print(x)
-x += 1
-print(x)
-#brak inkrementacji w języku python x++
+# print("Skrócone operatory: ")
+# x = 5
+# x = x+1
+# print(x)
+# x += 1
+# print(x)
+# #brak inkrementacji w języku python x++
 
-print("Konwersja typow: ")
-a = input("Podaj 1 liczbę: ") #input ma standardowo zaimplementowany String
-b = input("Podaj 2 liczbę: ")
-# print(int(a) + b) #to wygeneruje błąd
-print(int(a) + int(b)) #uzycie funkcji int
-print(float(a) + float(b)) # dla liczb zmiennoprzecinkowych
-y = 2
-z = 3
-print(y + z)
-print(str(y) + str(z)) #konwersja na string
 
-del y #pozbywanie się zmiennej. Przez poniższe wygenerował się błąd
-#print(str(y) + str(z))
+# print("Konwersja typow: ")
+# a = input("Podaj 1 liczbę: ") #input ma standardowo zaimplementowany String
+# b = input("Podaj 2 liczbę: ")
+# # print(int(a) + b) #to wygeneruje błąd
+# print(int(a) + int(b)) #uzycie funkcji int
+# print(float(a) + float(b)) # dla liczb zmiennoprzecinkowych
+# y = 2
+# z = 3
+# print(y + z)
+# print(str(y) + str(z)) #konwersja na string
 
-#grupowe przypisanie wartości zmiennej
-a=b=c = 2
-print(a, b, c)
+# del y #pozbywanie się zmiennej. Przez poniższe wygenerował się błąd
+# #print(str(y) + str(z))
+
+# #grupowe przypisanie wartości zmiennej
+# a=b=c = 2
+# print(a, b, c)
+# print(a + b + c) #dodanie liczb
